d9918738806418dabd1659eff4b1945aadee0039
Tafsi Bhuiyan <tafsib@nycstudents.net>
2023-12-15 10:47:40 -0500

    Second partner adding code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("hi this is the other partner adding some code");
+        System.out.println("this is the other partner adding some code");
     }
 }
